42d8ad7092edb2dec867bc9a0cb83961c4a0604b
mitya <kropotovd@gmail.com>
2018-06-05 01:41:49 +0300

    Exercise3

---
+++ src/test/java/lambda/part1/exercise/Exercise3.java
@@ -0,0 +1,74 @@
+package lambda.part1.exercise;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
+import lambda.data.Person;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+@SuppressWarnings({"ConstantConditions", "unused"})
+public class Exercise3 {
+
+    @Test
+    public void sortPersonsByAgeUsingArraysSortExpressionLambda() {
+        Person[] persons = getPersons();
+
+        // TODO использовать Arrays.sort + expression-lambda
+        Arrays.sort(persons, (person1, person2)->Integer.compare(person1.getAge(), person2.getAge()));
+
+        assertArrayEquals(new Person[]{
+            new Person("Иван", "Мельников", 20),
+            new Person("Николай", "Зимов", 30),
+            new Person("Алексей", "Доренко", 40),
+            new Person("Артем", "Зимов", 45)
+        }, persons);
+    }
+
+    @Test
+    public void sortPersonsByLastNameThenFirstNameUsingArraysSortExpressionLambda() {
+        Person[] persons = getPersons();
+
+        // TODO использовать Arrays.sort + statement-lambda
+        Arrays.sort(persons, (person1, person2)->{final String firstSirname = person1.getLastName();
+            final String secondSirname = person2.getLastName();
+            final String firstName = person1.getFirstName();
+            final String secondName = person2.getFirstName();
+            return firstSirname.compareTo(secondSirname)>0 ||
+                   firstSirname.compareTo(secondSirname)==0 && firstName.compareTo(secondName)>0? 1:
+                   firstSirname.compareTo(secondSirname)<0 ||
+                   firstSirname.compareTo(secondSirname)==0 && firstName.compareTo(secondName)<0? -1: 0;});
+
+        assertArrayEquals(new Person[]{
+            new Person("Алексей", "Доренко", 40),
+            new Person("Артем", "Зимов", 45),
+            new Person("Николай", "Зимов", 30),
+            new Person("Иван", "Мельников", 20)
+        }, persons);
+    }
+
+    @Test
+    public void findFirstWithAge30UsingGuavaPredicateLambda() {
+        List<Person> persons = Arrays.asList(getPersons());
+
+        // TODO использовать FluentIterable
+        Optional<Person> personOptional = FluentIterable.from(persons)
+                .firstMatch((person)->person.getAge()==30);
+        Person person = personOptional.get();
+
+        assertEquals(new Person("Николай", "Зимов", 30), person);
+    }
+
+    private Person[] getPersons() {
+        return new Person[]{
+            new Person("Иван", "Мельников", 20),
+            new Person("Алексей", "Доренко", 40),
+            new Person("Николай", "Зимов", 30),
+            new Person("Артем", "Зимов", 45)
+        };
+    }
+}
